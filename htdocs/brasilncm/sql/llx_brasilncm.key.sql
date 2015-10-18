782ab41e2fbff82c66ce066be675faf1c737ba16
Sasa Ostrouska <casaxa@gmail.com>
2015-10-18 02:15:03 -0300

    Started work on sql tables.

---
+++ htdocs/brasilncm/sql/llx_brasilncm.key.sql
@@ -0,0 +1,2 @@
+ALTER TABLE llx_brasil_ncm ADD INDEX idx_brasil_ncm;
+-- ALTER TABLE llx_brasil_ncm ADD CONSTRAINT fk_brasil_ncm_fk_customcode FOREIGN KEY (fk_customcode) REFERENCES llx_product (customcode) ;
